95880951b840e7f042cb93f21fc127b43ee98b46
RDavydiuk <romandaviduc@gmail.com>
2019-12-09 18:57:03 +0200

    first commit on test project !

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useRef, useEffect} from 'react';
+import './App.scss';
+import videojs from "video.js"
+// import {Main} from "./Main";
+import {CustomPlayer} from "./Player/Player";
+
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const defaultState: videojs.PlayerOptions = {
+        controls: false,
+        autoplay: true,
+        sources: [{
+            src: 'http://vjs.zencdn.net/v/oceans.mp4',
+            type: 'video/mp4'
+
+        }],
+        controlBar: {
+            fullscreenToggle: false,
+        },
+        height: 400,
+        width: 300,
+
+    };
+
+    const [options, setOptions] = useState<videojs.PlayerOptions>(defaultState);
+    const resize = () => {
+        setOptions({
+            ...options,
+            height: 800
+        })
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <CustomPlayer options={options} autoPlay={!!options.autoplay}/>
+
+              {/*<Main/>*/}
+
+                <button onClick={() => resize()}>stop auto play</button>
+            </header>
+
+        </div>
+    );
+};
 
 export default App;
--- src/Player/Player.tsx
+++ src/Player/Player.tsx
@@ -1,11 +1,79 @@
 import React, {useState, useRef, useEffect} from "react";
-import VideoPlayer from "video.js"
-export  const CustomPlayer = () =>{
+import videojs, {VideoJsPlayer} from "video.js"
 
 
 
+
+import 'video.js/dist/video-js.css';
+
+interface CustomPlayerInterface {
+    options: videojs.PlayerOptions,
+    autoPlay: boolean
+}
+
+
+export const CustomPlayer = (props: CustomPlayerInterface) => {
+    let [getPlayer, setPlayer] = useState<VideoJsPlayer | null>(null);
+    let videoRef = useRef<HTMLVideoElement | null>(null);
+
+
+    videojs.registerPlugin('myPlugin', function (options: any) {
+        let player = this;
+        player.on('play', () => {
+            console.log('play')
+        })
+        player.on("pause", () => {
+            console.log('pause')
+        })
+    });
+
+
+
+    videojs.use("video/mp4", (player: VideoJsPlayer) => {
+        return {
+            setSource: function (srcObj: any, next) {
+                // pass null as the first argument to indicate that the source is not rejected
+                console.log("middlaware", srcObj);
+                next(null, srcObj);
+            },
+        };
+    });
+
+    useEffect(() => {
+        console.log(props.options)
+        setPlayer(videojs(videoRef.current, {...props.options, plugins: {"myPlugin": {}}}, () => {
+        }));
+
+
+    }, []);
+
+    useEffect(() => {
+        if (getPlayer) {
+
+        }
+    }, [getPlayer]);
+
+    const handleTimeLine = (event: any) => {
+        console.log(event)
+    };
     return (
         <div>
+            <video ref={videoRef} className={"video-js"} onTimeUpdate={handleTimeLine}>
+
+            </video>
+            {getPlayer !== null && <h1>{getPlayer.duration()}</h1>}
+            <button onClick={() => {
+                getPlayer!.width(800)
+            }}>Resize
+            </button>
+            <input type={"range"} onChange={(data) => getPlayer!.volume(Number(data.target.value))}/>
+            <button onClick={() => getPlayer!.src(  `http://media.w3.org/2010/05/video/movie_300.mp4 ` )}>Change track
+            </button>
+            <button onClick={() => {getPlayer!.play()}}>Play</button>
+            <button onClick={() => getPlayer!.pause()}>Pause</button>
+            {/*<button onClick={()=>{ getPlayer! }}> </button>*/}
+            <form > </form>
+
 
         </div>
     )
